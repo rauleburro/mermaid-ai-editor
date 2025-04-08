4d1db4d5bd2a615696864508c4cf227147b74e44
Raul <rauleburro@gmail.com>
2025-04-08 17:58:40 +0200

    Remove Monaco Editor Webpack Plugin and Integrate Mermaid Initialization
    - Removed `monaco-editor-webpack-plugin` and `monaco-editor` dependencies from `package.json`.
    - Updated `CodeEditor.tsx` to import and initialize Mermaid with Monaco Editor using `monaco-mermaid`.
    - Added `useEffect` hook to initialize Mermaid when `monaco` is available.
    - Adjusted imports to include `useMonaco` from `@monaco-editor/react`.

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,13 +1,21 @@
 "use client";
 
 import { CodeContext } from "@/context/CodeContext";
-import React, { useContext } from "react";
-import MonacoEditor from "@monaco-editor/react";
+import React, { useContext, useEffect } from "react";
+import MonacoEditor, { useMonaco } from "@monaco-editor/react";
 import { useTheme } from "next-themes";
+import initMermaid from 'monaco-mermaid';
 
 export const CodeEditor = () => {
   const { code, setCode } = useContext(CodeContext);
   const { theme } = useTheme();
+  const monaco = useMonaco();
+
+  useEffect(() => {
+    if (monaco) {
+      initMermaid(monaco);
+    }
+  }, [monaco]);
 
   return (
     <MonacoEditor
