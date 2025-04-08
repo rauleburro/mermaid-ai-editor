be52715b4b85149c1f6cecf57d4250f2be4b6b81
Raul <rauleburro@gmail.com>
2025-04-08 17:22:16 +0200

    Add theme support to CodeEditor component
    - Imported `useTheme` from `next-themes` to access the current theme.
    - Applied conditional logic to set the editor's theme based on the user's theme preference.
    - Updated `CodeEditor.tsx` to dynamically switch between "vs-dark" and "light" themes using the `theme` variable.

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -3,15 +3,18 @@
 import { CodeContext } from "@/context/CodeContext";
 import React, { useContext } from "react";
 import MonacoEditor from "@monaco-editor/react";
+import { useTheme } from "next-themes";
 
 export const CodeEditor = () => {
   const { code, setCode } = useContext(CodeContext);
+  const { theme } = useTheme();
 
   return (
     <MonacoEditor
       height="100%"
       defaultLanguage="mermaid"
       value={code}
+      theme={theme === "dark" ? "vs-dark" : "light"}
       onChange={(value) => setCode(value || "")}
       options={{
         minimap: { enabled: false },
